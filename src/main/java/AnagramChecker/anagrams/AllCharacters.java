3dedd42285c3adbee4b01a7e6354d28d14f875f6
Allen Dungo <allendungo@Allens-MBP.broadband>
2022-11-30 16:48:30 +0000

    Add new implementations of Anagram Interface for future extensibility

---
+++ src/main/java/AnagramChecker/anagrams/AllCharacters.java
@@ -0,0 +1,11 @@
+package AnagramChecker.anagrams;
+
+import AnagramChecker.InputObject;
+
+public class AllCharacters implements Anagram {
+
+    @Override
+    public Boolean checkWords(InputObject inputObject) {
+        return null;
+    }
+}
---
+++ src/main/java/AnagramChecker/anagrams/WithDigits.java
@@ -0,0 +1,10 @@
+package AnagramChecker.anagrams;
+
+import AnagramChecker.InputObject;
+
+public class WithDigits implements Anagram {
+    @Override
+    public Boolean checkWords(InputObject inputObject) {
+        return null;
+    }
+}
